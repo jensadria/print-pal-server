c5ea10c8981e781ba7d9f89c6d391240af6c32fa
jensadria <jensadria@gmail.com>
2021-03-02 22:37:45 +0800

    changed start

--- index.js
+++ index.js
@@ -25,14 +25,14 @@ app.use('/api/products', products);
 app.use('/api/orders', orders);
 
 // Serve static assets if in production
-if (process.env.NODE_ENV === 'production') {
-  // Set static folder
-  app.use(express.static(__dirname + '/client/'));
+// if (process.env.NODE_ENV === 'production') {
+//   // Set static folder
+app.use(express.static(__dirname + '/client/dist'));
 
-  app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname + 'client/index.html'));
-  });
-}
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname + '/client/dist/index.html'));
+});
+// }
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Listening on port ${port}...`));
